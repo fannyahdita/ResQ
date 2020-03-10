0331d512d7226b7737585b7c1c9e678635b362ee
Fannyah Dita Cahya <fannyah.dita@ui.ac.id>
2020-03-10 15:58:39 +0700

    add actionBar, fonts, styling for button

--- app/src/main/java/com/tugasakhir/resq/MainActivity.kt
+++ app/src/main/java/com/tugasakhir/resq/MainActivity.kt
@@ -2,11 +2,18 @@ package com.tugasakhir.resq
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var actionBar : ActionBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        actionBar = this.supportActionBar!!
+        actionBar.title = "RES-Q"
+        actionBar.elevation = 0F
     }
 }
