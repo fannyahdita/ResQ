7c3342147b9428a9f2a8d7242458e70bc1bcd72a
Aviliani Pramestya <avipramestya14@gmail.com>
2020-03-11 11:25:12 +0700

    add BottomNavigationView in MainActivity

--- app/src/main/java/com/tugasakhir/resq/MainActivity.kt
+++ app/src/main/java/com/tugasakhir/resq/MainActivity.kt
@@ -2,11 +2,47 @@ package com.tugasakhir.resq
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: ActionBar
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_beranda -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_posko -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_temukan -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_kontak -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_akun -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
     }
+
 }
