a94e1cc33478318d5eabfd3103a764bbe505acd6
Fannyah Dita Cahya <fannyah.dita@ui.ac.id>
2020-03-15 17:10:25 +0700

    sign in implementation not yet done. still cannot logged in to the existing account

--- app/src/main/java/com/tugasakhir/resq/rescuer/SignInRescuerActivity.kt
+++ app/src/main/java/com/tugasakhir/resq/rescuer/SignInRescuerActivity.kt
@@ -2,16 +2,24 @@ package com.tugasakhir.resq.rescuer
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.tugasakhir.resq.MainActivity
 import com.tugasakhir.resq.OnboardingActivity
 import com.tugasakhir.resq.R
 import kotlinx.android.synthetic.main.activity_rescuer_signin.*
 
 class SignInRescuerActivity : AppCompatActivity() {
 
-    private lateinit var actionBar : ActionBar
+    private lateinit var actionBar: ActionBar
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +30,26 @@ class SignInRescuerActivity : AppCompatActivity() {
         actionBar.title = "BUAT AKUN"
         actionBar.elevation = 0F
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        authStateListener = FirebaseAuth.AuthStateListener {
+            val user : FirebaseUser? = it.currentUser
+
+            if(user != null) {
+                //user is signed in
+            } else {
+                //user is signed out
+            }
+        }
+
         button_signin_tosignup.setOnClickListener {
             val intent = Intent(this, SignUpRescuerActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        button_signin_finish.setOnClickListener {
+            isValidUser()
         }
     }
 
@@ -39,5 +63,51 @@ class SignInRescuerActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun isValidUser() {
+        val email = edittext_signin_email.text.toString().trim()
+        val password = textview_signin_password.text.toString().trim()
+
+        if (email.isEmpty()) {
+            edittext_signin_email.error = "Wajib diisi"
+            edittext_signin_email.requestFocus()
+            return
+        }
+
+        if (password.isEmpty()) {
+            edittext_signin_password.error = "Wajib diisi"
+            edittext_signin_password.requestFocus()
+            return
+        }
+
+        Log.wtf("SIGN IN: ", email)
+        Log.wtf("PASSWORD: ", password)
+        loggingInUser(email, password)
+    }
+
+    private fun loggingInUser(email: String, password: String) {
+        progressbar_signin.visibility = View.VISIBLE
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(
+                this
+            ) { p0 ->
+                if (p0.isSuccessful) {
+                    progressbar_signin.visibility = View.GONE
+                    Toast.makeText(
+                        this@SignInRescuerActivity, "Berhasil masuk",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    finish()
+                    val intent = Intent(this@SignInRescuerActivity, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    progressbar_signin.visibility = View.GONE
+                    Toast.makeText(
+                        this@SignInRescuerActivity, p0.exception?.message,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
     }
 }
