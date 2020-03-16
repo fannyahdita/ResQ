5130c108ffced53ceb527734f72bd1afc5dbd65b
Aviliani Pramestya <avipramestya14@gmail.com>
2020-03-16 23:19:21 +0700

    add login activity for korban

--- app/src/main/java/com/tugasakhir/resq/OnboardingActivity.kt
+++ app/src/main/java/com/tugasakhir/resq/OnboardingActivity.kt
@@ -3,6 +3,8 @@ package com.tugasakhir.resq
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.tugasakhir.resq.korban.BuatAkunActivity
+import com.tugasakhir.resq.korban.MasukActivity
 import kotlinx.android.synthetic.main.activity_onboarding.*
 
 class OnboardingActivity : AppCompatActivity() {
@@ -12,13 +14,13 @@ class OnboardingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_onboarding)
 
         button_onboard_victim_signup.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, BuatAkunActivity::class.java)
             startActivity(intent)
             finish()
         }
 
         button_onboard_rescuer_signup.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, MasukActivity::class.java)
             startActivity(intent)
             finish()
         }
