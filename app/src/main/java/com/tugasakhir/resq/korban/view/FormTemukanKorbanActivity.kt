db60316b5a50b90c40d1364d8be432e47df3849b
Aviliani Pramestya <avipramestya14@gmail.com>
2020-03-23 21:51:24 +0700

    implement increase and decrease button

--- app/src/main/java/com/tugasakhir/resq/korban/view/FormTemukanKorbanActivity.kt
+++ app/src/main/java/com/tugasakhir/resq/korban/view/FormTemukanKorbanActivity.kt
@@ -1,29 +1,97 @@
 package com.tugasakhir.resq.korban.view
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import com.tugasakhir.resq.R
+import kotlinx.android.synthetic.main.activity_temukansayaform_korban.*
 
-class FormTemukanKorbanFragment : Fragment() {
+class FormTemukanKorbanActivity : AppCompatActivity() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(
-            R.layout.activity_temukansayaform_korban,
-            container,
-            false
-        )
+    private lateinit var actionBar : ActionBar
+    var lansia_displayInt : Int = 0
+    var anak_displayInt : Int = 0
+    var dewasa_displayInt : Int = 0
+    var status: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temukansayaform_korban)
+
+        actionBar = this.supportActionBar!!
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.title = getString(R.string.temukansaya_actionbar)
+        actionBar.elevation = 0F
+
+        button_send.setOnClickListener {
+            val intent = Intent(this, Status1TemukanKorbanActivity::class.java)
+            startActivity(intent)
+        }
+
+        decrease_lansia.setOnClickListener {
+            if (lansia_displayInt != 0) {
+                lansia_displayInt--
+            }
+            status = 1
+            displayNumber(lansia_displayInt)
+        }
+
+        decrease_anak.setOnClickListener {
+            if (anak_displayInt != 0) {
+                anak_displayInt--
+            }
+            status = 3
+            displayNumber(anak_displayInt)
+        }
+
+        decrease_dewasa.setOnClickListener {
+            if (dewasa_displayInt != 0) {
+                dewasa_displayInt--
+            }
+            status = 2
+            displayNumber(dewasa_displayInt)
+        }
+
+        increase_lansia.setOnClickListener {
+            lansia_displayInt++
+            status = 1
+            displayNumber(lansia_displayInt)
+        }
+
+        increase_anak.setOnClickListener {
+            anak_displayInt++
+            status = 3
+            displayNumber(anak_displayInt)
+        }
+
+        increase_dewasa.setOnClickListener {
+            dewasa_displayInt++
+            status = 2
+            displayNumber(dewasa_displayInt)
+        }
     }
 
-    companion object {
-        fun newInstance() : FormTemukanKorbanFragment
-                = FormTemukanKorbanFragment()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun displayNumber(number: Int) {
+        if (status == 3) {
+            integer_number_anak.setText("" + number)
+        } else if (status == 2) {
+            integer_number_dewasa.setText("" + number)
+        } else {
+            integer_number_lansia.setText("" + number)
+        }
     }
 
 }
