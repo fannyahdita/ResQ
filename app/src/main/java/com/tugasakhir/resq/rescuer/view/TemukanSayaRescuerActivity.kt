713414c5cff93d37c18d7fd7af07f2fc9373da73
Fannyah Dita Cahya <fannyah.dita@ui.ac.id>
2020-03-25 18:10:18 +0700

    implement maps layout and put multiple markers on the map

--- app/src/main/java/com/tugasakhir/resq/rescuer/view/TemukanSayaRescuerActivity.kt
+++ app/src/main/java/com/tugasakhir/resq/rescuer/view/TemukanSayaRescuerActivity.kt
@@ -2,15 +2,32 @@ package com.tugasakhir.resq.rescuer.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.tugasakhir.resq.MainActivity
 import com.tugasakhir.resq.R
+import com.tugasakhir.resq.korban.model.Korban
+import kotlinx.android.synthetic.main.activity_temukansaya_rescuer.*
 
 class TemukanSayaRescuerActivity : AppCompatActivity() {
 
-    private lateinit var actionBar : ActionBar
+    private lateinit var actionBar: ActionBar
+    private lateinit var mapFragment: SupportMapFragment
+    private lateinit var googleMap: GoogleMap
+    private lateinit var korban: Korban
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +38,41 @@ class TemukanSayaRescuerActivity : AppCompatActivity() {
         actionBar.title = getString(R.string.temukansaya_actionbar)
         actionBar.elevation = 0F
 
+        layout_detail_marker.visibility = View.GONE
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.fragment_map_rescuer)
+                as SupportMapFragment
+
+        FirebaseDatabase.getInstance().getReference("InfoKorban")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(p0: DataSnapshot) {
+                    val children = p0.children
+                    children.forEach {
+                        val uid = it.key.toString()
+                        val latitude = it.child("latitude").value.toString().toDouble()
+                        val longitude = it.child("longitude").value.toString().toDouble()
+                        val victimId = it.child("idKorban").value.toString()
+
+                        Log.d("MAPSSS : ", "$latitude, $longitude, $victimId")
+                        setMaps(latitude, longitude, victimId, uid)
+                    }
+                }
+
+                override fun onCancelled(p0: DatabaseError) {
+                    Log.d("TemukanSayaError : ", p0.message)
+                }
+            })
     }
+
+    private fun setMaps(latitude: Double, longitude: Double, victimId: String, uid: String) {
+        mapFragment.getMapAsync {
+            googleMap = it
+            val location = LatLng(latitude, longitude)
+            googleMap.addMarker(MarkerOptions().position(location).title(victimId))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLng(location))
+        }
+    }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
