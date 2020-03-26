3c42472faaa2f7e48da33b8b0a054e73b8480b50
Aviliani Pramestya <avipramestya14@gmail.com>
2020-03-26 22:13:35 +0700

    implement button cancel for temukan saya korban

--- app/src/main/java/com/tugasakhir/resq/korban/view/StatusAcceptedFragment.kt
+++ app/src/main/java/com/tugasakhir/resq/korban/view/StatusAcceptedFragment.kt
@@ -1,11 +1,14 @@
 package com.tugasakhir.resq.korban.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.tugasakhir.resq.MainActivity
 import com.tugasakhir.resq.R
+import kotlinx.android.synthetic.main.fragment_temukansayastatus1_korban.*
 
 class StatusAcceptedFragment : Fragment() {
 
@@ -17,6 +20,15 @@ class StatusAcceptedFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_temukansayastatus2_korban, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        button_batalkan.setOnClickListener {
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+            activity!!.finish()
+        }
+    }
     companion object {
         fun newInstance(): StatusAcceptedFragment = StatusAcceptedFragment()
     }
--- app/src/main/java/com/tugasakhir/resq/korban/view/StatusSentFragment.kt
+++ app/src/main/java/com/tugasakhir/resq/korban/view/StatusSentFragment.kt
@@ -1,11 +1,15 @@
 package com.tugasakhir.resq.korban.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.tugasakhir.resq.MainActivity
 import com.tugasakhir.resq.R
+import kotlinx.android.synthetic.main.fragment_temukansayastatus1_korban.*
 
 class StatusSentFragment : Fragment() {
 
@@ -15,6 +19,29 @@ class StatusSentFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_temukansayastatus1_korban, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        button_batalkan.setOnClickListener {
+
+            val builder = AlertDialog.Builder(activity!!)
+            builder.setTitle("Batalkan Permintaan Bantuan")
+            builder.setMessage("Bantuan Anda akan segera ditangani, apakah Anda yakin ingin membatalkan?")
+            builder.setNegativeButton("Tetap Batalkan"){_,_ ->
+                val intent = Intent(activity, MainActivity::class.java)
+                startActivity(intent)
+                activity!!.finish()
+            }
+
+            builder.setPositiveButton("Jangan Batalkan"){_,_ ->
+
+            }
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+
+        }
     }
 
     companion object {
