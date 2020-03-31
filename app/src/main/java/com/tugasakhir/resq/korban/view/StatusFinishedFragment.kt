a03862b3fda06b2bf9957db3be2b50540188d1b1
Aviliani Pramestya <avipramestya14@gmail.com>
2020-03-31 10:04:13 +0700

    finishing all fragment from status activity

--- app/src/main/java/com/tugasakhir/resq/korban/view/StatusFinishedFragment.kt
+++ app/src/main/java/com/tugasakhir/resq/korban/view/StatusFinishedFragment.kt
@@ -18,7 +18,6 @@ import com.tugasakhir.resq.R
 import com.tugasakhir.resq.korban.model.InfoKorban
 import kotlinx.android.synthetic.main.fragment_temukansayaselesai_korban.*
 
-
 class StatusFinishedFragment : Fragment() {
 
     override fun onCreateView(
@@ -35,6 +34,7 @@ class StatusFinishedFragment : Fragment() {
         val rescuerName = arguments!!.getString("rescuerName")
         val rescuerPhone = arguments!!.getString("rescuerPhone")
         val idInfoKorban = arguments!!.getString("idInfoKorban")
+        val idKorbanTertolong = arguments!!.getString("idKorbanTertolong")
 
         getKorbanData(idInfoKorban)
 
@@ -44,19 +44,20 @@ class StatusFinishedFragment : Fragment() {
         button_selesai.setOnClickListener {
             val user = FirebaseAuth.getInstance().currentUser?.uid
             FirebaseDatabase.getInstance().reference.child("AkunKorban/$user").child("askingHelp").setValue(false)
+            FirebaseDatabase.getInstance().reference.child("KorbanTertolong/$idKorbanTertolong").child("finished").setValue(true)
 
             val intent = Intent(activity, MainActivity::class.java)
             startActivity(intent)
             activity!!.finish()
-
         }
     }
     companion object {
-        fun newInstance(rescuerPhone: String, rescuerName: String, idInfoKorban: String): StatusFinishedFragment {
+        fun newInstance(rescuerName: String, rescuerPhone: String, idInfoKorban: String, idKorbanTertolong: String): StatusFinishedFragment {
             val args = Bundle()
             args.putString("rescuerName", rescuerName)
             args.putString("rescuerPhone", rescuerPhone)
             args.putString("idInfoKorban", idInfoKorban)
+            args.putString("idKorbanTertolong", idKorbanTertolong)
             val fragment = StatusFinishedFragment()
             fragment.arguments = args
             return fragment
@@ -64,25 +65,30 @@ class StatusFinishedFragment : Fragment() {
     }
 
     private fun getKorbanData(idInfoKorban: String?) {
-        val ref = FirebaseDatabase.getInstance().reference.child("Rescuers/$idInfoKorban")
+        val ref = FirebaseDatabase.getInstance().reference.child("InfoKorban/$idInfoKorban")
         val rescuerFragment = object : ValueEventListener {
             override fun onDataChange(p0: DataSnapshot) {
-                val infoKorban = p0.getValue(InfoKorban::class.java)
-                if (infoKorban?.bantuanEvakuasi.toString() == "true") {
+
+                if (p0.child("bantuanEvakuasi").value.toString() == "true") {
                     textview_jenis_bantuan_val.text = getString(R.string.rb_evakuasi)
-                } else if (infoKorban?.bantuanMakanan.toString() == "true") {
+                } else if (p0.child("bantuanMakanan").value.toString() == "true") {
                     textview_jenis_bantuan_val.text = getString(R.string.rb_makanan)
-                } else {
+                } else if (p0.child("bantuanMedis").value.toString() == "true") {
                     textview_jenis_bantuan_val.text = getString(R.string.rb_medis)
                 }
                 textview_jumlah_lansia.text =
-                    Html.fromHtml(getString(R.string.number_of_elderly, infoKorban?.jumlahLansia.toString()))
+                    Html.fromHtml(getString(R.string.number_of_elderly, p0.child("jumlahLansia").value.toString()))
                 textview_jumlah_dewasa.text =
-                    Html.fromHtml(getString(R.string.number_of_adults, infoKorban?.jumlahDewasa.toString()))
+                    Html.fromHtml(getString(R.string.number_of_adults, p0.child("jumlahDewasa").value.toString()))
                 textview_jumlah_anak.text =
-                    Html.fromHtml(getString(R.string.number_of_children, infoKorban?.jumlahAnak.toString()))
+                    Html.fromHtml(getString(R.string.number_of_children, p0.child("jumlahAnak").value.toString()))
 
-                textview_info_tambahan_val.text = infoKorban?.infoTambahan
+                Log.d("INFOKORBAN JUMLAH LANSIA ", p0.child("jumlahLansia").value.toString())
+                Log.d("INFOKORBAN JUMLAH DEWASA ", p0.child("jumlahDewasa").value.toString())
+                Log.d("INFOKORBAN JUMLAH ANAK ", p0.child("jumlahAnak").value.toString())
+                Log.d("INFOKORBAN INFO TAMBAHAN ", p0.child("infoTambahan").value.toString())
+
+                textview_info_tambahan_val.text = p0.child("infoTambahan").value.toString()
             }
 
             override fun onCancelled(p0: DatabaseError) {
