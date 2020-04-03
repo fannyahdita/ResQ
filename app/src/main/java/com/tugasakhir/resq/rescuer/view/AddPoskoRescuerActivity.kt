f8e32d8eaebb09ce2908f0936e912469fad4e0ea
Fannyah Dita Cahya <fannyah.dita@ui.ac.id>
2020-04-03 09:27:52 +0700

    fix import on posko

--- app/src/main/java/com/tugasakhir/resq/rescuer/view/AddPoskoRescuerActivity.kt
+++ app/src/main/java/com/tugasakhir/resq/rescuer/view/AddPoskoRescuerActivity.kt
@@ -23,7 +23,6 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.tugasakhir.resq.R
