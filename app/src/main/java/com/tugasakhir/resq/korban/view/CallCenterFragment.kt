7e922ec0a36528e2104b0edace92b5dc21839944
Aviliani Pramestya <avipramestya14@gmail.com>
2020-04-18 16:43:20 +0700

    change adapter implementation in call center fragment

--- app/src/main/java/com/tugasakhir/resq/korban/view/CallCenterFragment.kt
+++ app/src/main/java/com/tugasakhir/resq/korban/view/CallCenterFragment.kt
@@ -5,15 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tugasakhir.resq.R
 import com.tugasakhir.resq.korban.CallCenterAdapter
+import com.tugasakhir.resq.korban.CallCenterSarAdapter
 import com.tugasakhir.resq.korban.model.CallCenter
 import kotlinx.android.synthetic.main.fragment_call_center.*
 
 class CallCenterFragment : Fragment() {
-    var callCenterList = ArrayList<CallCenter>()
-    var sarCallCenterList = ArrayList<CallCenter>()
-    var adapter: CallCenterAdapter? = null
+    var callCenterList = ArrayList<CallCenter?>()
+    var sarCallCenterList = ArrayList<CallCenter?>()
+    private val callCenterAdapter = CallCenterAdapter()
+    private val sarCallCenterAdapter = CallCenterSarAdapter()
 
     companion object {
         fun newInstance(): CallCenterFragment =
@@ -31,6 +35,11 @@ class CallCenterFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        list_call_center.layoutManager = GridLayoutManager(activity, 3, LinearLayoutManager.VERTICAL, false)
+        list_call_center.adapter = callCenterAdapter
+
+        list_call_center_sar.layoutManager = GridLayoutManager(activity, 1, LinearLayoutManager.VERTICAL, false)
+        list_call_center_sar.adapter = sarCallCenterAdapter
 
         callCenterList.add(CallCenter("Ambulans 1", "118"))
         callCenterList.add(CallCenter("Ambulans 2", "119"))
@@ -39,8 +48,7 @@ class CallCenterFragment : Fragment() {
         callCenterList.add(CallCenter("PLN", "123"))
         callCenterList.add(CallCenter("Polisi", "110"))
 
-        adapter = CallCenterAdapter(context, callCenterList, 1)
-        grid_view_call_center.adapter = adapter
+        callCenterAdapter.setCallCenter(callCenterList)
 
         sarCallCenterList.add(CallCenter("Call Center 1", "021-5501512"))
         sarCallCenterList.add(CallCenter("Call Center 2", "021-550511111"))
@@ -49,9 +57,13 @@ class CallCenterFragment : Fragment() {
         sarCallCenterList.add(CallCenter("Jakarta Pusat", "021-6344215"))
         sarCallCenterList.add(CallCenter("Jakarta Barat", "021-5682284"))
         sarCallCenterList.add(CallCenter("Jakarta Utara", "021-43931063"))
+        sarCallCenterList.add(CallCenter("Jakarta Selatan", "021-7515054"))
+        sarCallCenterList.add(CallCenter("Jakarta Timur", "021-85904904"))
+        sarCallCenterList.add(CallCenter("Jakarta Pusat", "021-6344215"))
+        sarCallCenterList.add(CallCenter("Jakarta Barat", "021-5682284"))
+        sarCallCenterList.add(CallCenter("Jakarta Utara", "021-43931063"))
 
-        adapter = CallCenterAdapter(context, sarCallCenterList, 2)
-        grid_view_call_center_sar.adapter = adapter
+        sarCallCenterAdapter.setCallCenter(sarCallCenterList)
     }
 
 }
