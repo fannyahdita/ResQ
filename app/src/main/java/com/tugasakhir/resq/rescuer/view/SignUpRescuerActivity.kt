a4be6392cf73cc408e2a72eb27ffb5b42b6c4baa
Fannyah Dita Cahya <fannyah.dita@ui.ac.id>
2020-05-01 15:50:06 +0700

    fix error when sign up rescuer

--- app/src/main/java/com/tugasakhir/resq/rescuer/view/SignUpRescuerActivity.kt
+++ app/src/main/java/com/tugasakhir/resq/rescuer/view/SignUpRescuerActivity.kt
@@ -83,7 +83,7 @@ class SignUpRescuerActivity : AppCompatActivity() {
         phone: String, employeeId: String,
         instansi: String, division: String
     ) {
-        val id = FirebaseAuth.getInstance().currentUser!!.uid
+        val id = FirebaseAuth.getInstance().uid.toString()
         progressbar_signup.visibility = View.VISIBLE
         button_signup_finish.isClickable = false
         button_signup_finish.setBackgroundResource(R.drawable.shape_filled_button_clicked)
