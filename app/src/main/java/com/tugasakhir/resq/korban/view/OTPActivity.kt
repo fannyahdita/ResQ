809d5ccc9abf3f717ca28bd85098fe3da7f8819a
Aviliani Pramestya <avipramestya14@gmail.com>
2020-05-10 17:04:57 +0700

    checking new user

--- app/src/main/java/com/tugasakhir/resq/korban/view/OTPActivity.kt
+++ app/src/main/java/com/tugasakhir/resq/korban/view/OTPActivity.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -17,8 +18,13 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.tugasakhir.resq.base.view.MainActivity
 import com.tugasakhir.resq.R
+import com.tugasakhir.resq.base.view.HomeFragment
 import kotlinx.android.synthetic.main.activity_korban_otp.*
 import java.util.concurrent.TimeUnit
 
@@ -166,45 +172,39 @@ class OTPActivity : AppCompatActivity() {
         mAuth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("PHONE AUTH OTP", "signInWithCredential:success")
-                    Log.d(
-                        "PHONE AUTH OTP",
-                        "CREATED AT 1 " + (task.result?.user?.metadata?.creationTimestamp).toString()
-                    )
-                    Log.d(
-                        "PHONE AUTH OTP",
-                        "CREATED AT 2 " + (task.result?.user?.metadata?.lastSignInTimestamp).toString()
-                    )
 
-                    if (task.result?.user?.metadata?.creationTimestamp != task.result?.user?.metadata?.lastSignInTimestamp) {
-                        progressbar_otp.visibility = View.GONE
-                        val intent = Intent(this, MainActivity::class.java)
-                        intent.flags =
-                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                        startActivity(intent)
-                        finish()
+                    val user = task.result?.user?.uid
 
-                        Log.d("PHONE AUTH OTP", "AKUN LAMA INI YAAA")
-                    } else {
-                        progressbar_otp.visibility = View.GONE
-                        val intent = Intent(this, UserNamaActivity::class.java)
-                        intent.putExtra(EXTRA_PHONE, phone)
-                        intent.flags =
-                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                        startActivity(intent)
-                        finish()
+                    FirebaseDatabase.getInstance().reference.child("AkunKorban/$user")
+                        .addValueEventListener(object : ValueEventListener {
+                            override fun onDataChange(p0: DataSnapshot) {
+                                if (p0.exists()) {
+                                    progressbar_otp.visibility = View.GONE
+                                    openActivity(false, phone)
+                                } else {
+                                    progressbar_otp.visibility = View.GONE
+                                    openActivity(true, phone)
+                                }
+                            }
 
-                        Log.d("PHONE AUTH OTP", "AKUN BARU YAAA")
-                    }
+                            override fun onCancelled(p0: DatabaseError) {
+                                Log.d("DatabaseReference : ", "user with id $user is not exist")
+                            }
+                        })
 
-                    val user = task.result?.user
-                    // ...
                 } else {
                     // false OTP input
                     progressbar_otp.visibility = View.GONE
                     button_sendotp.isClickable = true
                     button_sendotp.setBackgroundResource(R.drawable.shape_filled_button)
+                    val builder = AlertDialog.Builder(this@OTPActivity)
+                    builder.setTitle("Verifikasi Gagal")
+                    builder.setMessage("Pastikan kode verifikasi yang Anda masukkan benar")
+                    builder.setNeutralButton("Oke"){_,_ ->
+
+                    }
+                    val dialog: AlertDialog = builder.create()
+                    dialog.show()
                     Log.w("PHONE AUTH OTP", "signInWithCredential:failure", task.exception)
                     if (task.exception is FirebaseAuthInvalidCredentialsException) {
                         // The verification code entered was invalid
@@ -213,12 +213,31 @@ class OTPActivity : AppCompatActivity() {
             }
     }
 
+    private fun openActivity(isNew: Boolean, phone: String) {
+        if (isNew) {
+            val intent = Intent(this, UserNamaActivity::class.java)
+            intent.putExtra(EXTRA_PHONE, phone)
+            intent.flags =
+                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+
+            Log.d("PHONE AUTH OTP", "AKUN BARU YAAA")
+        } else {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags =
+                Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+
+            Log.d("PHONE AUTH OTP", "AKUN LAMA INI YAAA")
+        }
+    }
+
     private fun timer(millisInFuture:Long,countDownInterval:Long): CountDownTimer {
         return object: CountDownTimer(millisInFuture,countDownInterval){
             override fun onTick(millisUntilFinished: Long){
                 count_down_timer.text = (millisUntilFinished / 1000).toString()
-//                count_down_timer.text =
-//                    Html.fromHtml(getString(R.string.cdt_otp, (millisUntilFinished / 1000).toString()))
             }
 
             override fun onFinish() {
